080b538866b11e3582781367c949b8c1ffa1b529
Rituraj Paul <ronnieshro@gmail.com>
2025-04-24 21:01:26 +0530

    UserModel Edited

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,4 +1,4 @@
-import UserModel from "../Models/userModel.js";
+import UserModel from "../Models/UserModel.js";
 import { Webhook } from "svix";
 import getRawBody from "raw-body";
 import dotenv from "dotenv";
