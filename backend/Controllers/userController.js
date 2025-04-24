924e6977ecb37bb60b6d0513d778e9493c70d344
Rituraj Paul <ronnieshro@gmail.com>
2025-04-24 23:02:06 +0530

    edited

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -30,12 +30,14 @@ const clerkWebhooks = async (req, resp) => {
     const { data, type } = evt;
     console.log("📨 Clerk Webhook Type:", type);
     console.log("👤 Clerk Webhook Data:", data);
+    const email = data.email_addresses?.[0]?.email_address|| "No Email Found",
     switch (type) {
       // Creating a new user
+
       case "user.created": {
         const userData = {
           clerkId: data.id, // the id of the clerk
-          email: data.email_addresses[0].email_address,
+          email: email,
           firstName: data.first_name,
           lastName: data.last_name,
           photo: data.image_url,
